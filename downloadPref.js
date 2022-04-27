5422d9d7c64aea0d87f63863319e5a63e7f87c45
Taisuke Fukuno <fukuno@jig.jp>
2022-04-28 07:03:08 +0900

    add pref.cbor

--- downloadPref.js
+++ downloadPref.js
@@ -1,11 +1,17 @@
 import { fetchZip } from "./fetchZip.js";
 import { CSV } from "https://js.sabae.cc/CSV.js";
+import { CBOR } from "https://js.sabae.cc/CBOR.js";
 
 const prefurl = "http://gov-csv-export-public.s3.ap-northeast-1.amazonaws.com/mt_pref/mt_pref_all.csv.zip";
 
 const bin = await fetchZip(prefurl);
 await Deno.writeFile("data/pref.csv", bin);
 const txt = new TextDecoder().decode(bin);
-const json = CSV.toJSON(CSV.decode(txt));
+const csv = CSV.decode(txt);
+const json = CSV.toJSON(csv);
 await Deno.writeTextFile("data/pref.json", JSON.stringify(json, null, "\t"));
+await Deno.writeTextFile("data/pref.min.json", JSON.stringify(json));
+await Deno.writeTextFile("data/pref.csv.json", JSON.stringify(csv));
 console.log(json);
+await Deno.writeFile("data/pref.csv.cbor", CBOR.encode(csv));
+await Deno.writeFile("data/pref.cbor", CBOR.encode(json));
